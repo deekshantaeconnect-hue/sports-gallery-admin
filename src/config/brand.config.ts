2f0931ab6e9799fc0bd06d7d4d82ad03b7ea2757
deekshantaeconnect-hue <deekshant.aeconnect@gmail.com>
2026-07-29 22:53:48 +0530

    Update brand.config.ts

--- src/config/brand.config.ts
+++ src/config/brand.config.ts
@@ -5,6 +5,8 @@ export const BRAND = {
   useStoreName: "sports-gallery-store",
 shortName: "AE",
     fullname:"Sports Gallery Pvt Ltd",
+    title:"Nature’s Finest Products",
+
 
   // Replace with your own logo URL (Cloudinary or public image URL)
   logo: "https://thumbs.dreamstime.com/b/vector-sports-icons-circle-icon-sign-symbol-pictogram-set-collection-flat-style-isolated-different-sport-equipment-games-66247271.jpg",
